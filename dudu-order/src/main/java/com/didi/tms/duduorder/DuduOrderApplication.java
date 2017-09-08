8dfe460d9d96b5dc618d44fb1ecb61215423d345
ivanyu <yobuke@126.com>
2017-09-08 11:21:03 +0800

    add order model and eureka server model.

---
+++ dudu-order/src/main/java/com/didi/tms/duduorder/DuduOrderApplication.java
@@ -0,0 +1,12 @@
+package com.didi.tms.duduorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuduOrderApplication.class, args);
+    }
+}
