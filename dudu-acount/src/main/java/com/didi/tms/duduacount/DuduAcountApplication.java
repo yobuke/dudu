19f4fb9c8774090ec266c95d8cb0ec6ba45a7d61
余志立 <yobuke@126.com>
2017-09-08 10:57:52 +0800

    upload
    create

---
+++ dudu-acount/src/main/java/com/didi/tms/duduacount/DuduAcountApplication.java
@@ -0,0 +1,12 @@
+package com.didi.tms.duduacount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduAcountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuduAcountApplication.class, args);
+	}
+}
